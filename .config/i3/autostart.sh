29af3152a68ffc0d20c6fb6f045e3fb7374747bd
Rendi Wijiatmoko <rendi.os@brins.co.id>
2022-09-20 11:00:24 +0700

    change background

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -3,7 +3,7 @@
 # /usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1 &
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 # nitrogen --restore; sleep 1; 
-feh --bg-fill ~/.config/backgrounds/wallhaven-y8o51x_1920x1080.png &
+feh --bg-fill ~/.config/backgrounds/wallhaven-lmxmxy.png &
 picom -b &
 numlockx on &
 nm-applet &
